4016820600fa7b0b85ff08efe30b5c0efc9937fe
Nick Sellen <git@nicksellen.co.uk>
2016-03-16 02:32:42 +0100

    Added functions to manage docker postgres/redis
    There are 4 commands:
      docker-up     - initialize containers
      docker-down   - destroy containers
      docker-start  - start containers
      docker-stop   - stop containers
    
    To let the Makefile postgres commands work you need to create/edit
    local_settings.make and ensure it has the line:
    
      pg = $(1) -h localhost -p 5432 -U $(pg_user) -w

--- env.sh
+++ env.sh
@@ -14,3 +14,52 @@ function ymake() {
 function ypm2() {
   (cd $base && pm2 "$@")
 }
+
+function container-exists() {
+  docker inspect $1 >/dev/null 2>&1
+}
+
+function wait-for-postgres() {
+  while ! psql -h localhost -p 5432 -w -U postgres -c 'select 1' >/dev/null 2>&1; do
+    echo -n .
+    sleep 1
+  done
+  echo " connected!"
+}
+
+function docker-up() {
+
+  if ! container-exists yunity-redis; then
+    docker run -p 6379:6379 --name yunity-redis -d redis
+  fi
+
+  if ! container-exists yunity-postgres; then
+    echo "initialing postgres!"
+    docker run -p 5432:5432 --name yunity-postgres -e POSTGRES_PASSWORD= -d postgres
+    echo -n "Waiting for postgres to start..."
+    wait-for-postgres
+  fi
+
+  ymake migrate-db
+
+}
+
+function docker-start() {
+  docker start yunity-postgres
+  docker start yunity-redis
+}
+
+function docker-stop() {
+  docker stop yunity-postgres
+  docker stop yunity-redis
+}
+
+function docker-down() {
+  if container-exists yunity-postgres; then
+    docker rm -f yunity-postgres
+  fi
+  if container-exists yunity-redis; then
+    docker rm -f yunity-redis
+  fi
+}
+
